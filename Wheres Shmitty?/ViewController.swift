5e7a497fde4e599f0987be8aca559e08a8df586d
Ahmed Shamim <ahmed@fusi.io>
2015-12-23 14:55:40 -0500

    Initial Commit

---
+++ Wheres Shmitty?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wheres Shmitty?
+//
+//  Created by Ahmed Shamim on 12/23/15.
+//  Copyright (c) 2015 Ahmed Shamim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
